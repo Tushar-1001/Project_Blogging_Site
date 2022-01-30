a246a0bd856cf27f75750cde40f6ccdcff058c4e
Tushar-1001 <88825224+Tushar-1001@users.noreply.github.com>
2022-01-30 13:57:43 +0530

    Final

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+const route = require("./routes/route.js");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    "mongodb+srv://monty-python:SnYUEY4giV9rekw@functionup-backend-coho.0zpfv.mongodb.net/Tushar_Saini?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Project Blogging Site - Database Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(3000, function () {
+  console.log("Express app running on port " + 3000);
+});
